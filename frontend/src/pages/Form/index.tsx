e41583e7cb4bb212482ac45b96d2f0eeb36c6f5e
edsonlr <elribeirom10@gmail.com>
2022-05-11 19:30:27 -0300

    dsflight Form Layout

--- frontend/src/pages/Form/index.tsx
+++ frontend/src/pages/Form/index.tsx
@@ -1,9 +1,46 @@
+import './styles.css';
+
 function Form() {
 
+    const flight = {
+        id: 1,
+        image: " http://www.geocities.ws/rumboalsud/B737/13-cx-vhm.jpg",
+        title: " Pluna – 1970 - CX-BHM",
+        count: 2,
+        score: 4.5
+    };
+
+
     return (
-        <h1>Página de formulario</h1>
+        <div className="dsflight-form-container">
+            <img className="dsflight-flight-card-image" src={flight.image} alt={flight.title} />
+            <div className="dsflight-card-bottom-container">
+                <h3>{flight.title}</h3>
+                <form className="dsflight-form">
+                    <div className="form-group dsflight-form-group">
+                        <label htmlFor="email">Informe seu email</label>
+                        <input type="email" className="form-control" id="email" />
+                    </div>
+                    <div className="form-group dsflight-form-group">
+                        <label htmlFor="score">Informe sua avaliação</label>
+                        <select className="form-control" id="score">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                        </select>
+                    </div>
+                    <div className="dsflight-form-btn-container">
+                        <button type="submit" className="btn btn-primary dsflight-btn">Salvar</button>
+                    </div>
+                </form >
+                <button className="btn btn-primary dsflight-btn mt-3">Cancelar</button>
+            </div >
+        </div >
+
     );
-    
-    }
-    
-    export default Form;
+
+}
+
+export default Form;
