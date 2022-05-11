3db00e5365584e36dd65e38c82738870d1b85c15
edsonlr <elribeirom10@gmail.com>
2022-05-11 11:37:06 -0300

    Projeto Flight Criado

---
+++ backend/src/test/java/com/devsuperior/dsflight/DsflightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsflight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsflightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
